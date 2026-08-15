d67a0149bd6467a522662506fa3c52496bd6e9c3
ABayly89 <ash.bayly@gmail.com>
2026-08-15 12:58:45 +1200

    Big push - Auto L/D mode
    reverted website colours to black on white, and then created an auto dark mode that follows user device preference. created an SOP for reviewing adding to avoid any stuff ups. published second "review"

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 const button = document.getElementById("hateButton");
 const aboutText = document.querySelectorAll(".aboutText");
 
-button.addEventListener("click", function() {
+if (button) {
 
-    if (button.textContent === "Okay, I Take It Back") {
+    button.addEventListener("click", function() {
 
-        aboutText.forEach(function(paragraph) {
-            paragraph.textContent = "This website exists because I wanted to learn web development while reviewing music that I sincerely enjoy. Chat GPT is my best friend in the whole world.";
-        });
+        if (button.textContent === "Okay, I Take It Back") {
 
-        button.textContent = "I HATE THIS ABOUT PAGE";
+            aboutText.forEach(function(paragraph) {
+                paragraph.textContent = "This website exists because I wanted to learn web development while reviewing music that I sincerely enjoy. Chat GPT is my best friend in the whole world.";
+            });
 
-    } else {
+            button.textContent = "I HATE THIS ABOUT PAGE";
 
-        aboutText.forEach(function(paragraph) {
-            paragraph.textContent = "I fucking hate this about page";
-        });
+        } else {
 
-        button.textContent = "Okay, I Take It Back";
-    }
+            aboutText.forEach(function(paragraph) {
+                paragraph.textContent = "I fucking hate this about page";
+            });
 
-});
+            button.textContent = "Okay, I Take It Back";
+        }
+
+    });
+
+}
+
+
+const albums = [
+    "Songs for the Deaf",
+    "Who Let the Dogs Out",
+    "Little Miss Twain"
+];
+
+const randomButton = document.getElementById("randomButton");
+const randomAlbum = document.getElementById("randomAlbum");
+
+if (randomButton) {
+
+    randomButton.addEventListener("click", function() {
+
+        const randomIndex = Math.floor(Math.random() * albums.length);
+
+        randomAlbum.textContent = albums[randomIndex];
+
+    });
+
+}
